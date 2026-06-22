db6161ca99ec7e588ca3511ebc32997d10943967
subekminecraft-source <subekminecraft@gmail.com>
2026-06-22 19:08:19 +0200

    Update Supabase URL in auth-config.js

--- auth-config.js
+++ auth-config.js
@@ -3,7 +3,7 @@
  * Dopóki enabled: false — działa tryb demo w przeglądarce.
  */
 window.AmiQAuthConfig = {
-  supabaseUrl: "https://cgqutdfznvxijpxputfn.supabase.co",
+  supabaseUrl: "https://cgqutdfznvxijpxputfn.supabase.co/rest/v1/",
   supabaseAnonKey: "sb_secret_D1fN5ZMURTLiulSXL1gx-g_OJ2tunw5",
   enabled: true
 };
